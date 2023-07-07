04ef456d54c692f01a6c95995eda23613c9bbcc3
aps <aravindpsabu@gmail.com>
2023-07-07 11:25:20 +0530

    Updated OF in PowerFinder

--- src/main/java/powerpackage/PowerFinder.java
+++ src/main/java/powerpackage/PowerFinder.java
@@ -1,9 +1,9 @@
 package powerpackage;
 
-public class powerfinder {
+public class PowerFinder {
     public static int OF(int base, int exponent) {
         int pow = 1;
-        for (int i = 1; i<exponent; i++) {
+        for (int i = 1; i<=exponent; i++) {
             pow *= base;
         }
         return pow;
